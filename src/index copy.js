3577cd8120fd1f906622a0c9116f4eaf1b46c7ec
Herbrand Hofker <herbrand.hofker@GMAIL.COM>
2022-04-30 09:51:30 +0200

    a

--- src/index copy.js
+++ src/index copy.js
@@ -16,8 +16,6 @@ en.addEventListener("click", () => changeLanguage("en"))
 const title = document.getElementById("title");
 const subtitle = document.getElementById("subtitle");
 const goal = document.getElementById("goal");
-const course_title = document.getElementById("course_title");
-const course_text = document.getElementById("course_text");
 changeLanguage("nl");
 function changeLanguage(lang) {
     var language = {
@@ -26,55 +24,43 @@ function changeLanguage(lang) {
                 "Superfluid Energy for energy independent houses",
             subtitle:
                 "Quality installations on solar, heatpump and batteries",
-              course_title:"Cursus zonnepanelen installeren voor Oekraïners",
-            course_text:"Superfuid Energy organiseert samen met Solar Techniek Nederland een cursus Zonnepanelen installeren speciaal voor Oekraïners, met de garantie om binnen 6 weken een goede baan in Nederland te hebben."
-    
+            goal:
+                "Get rid of russian gas",
+            subgoal:
+                "Prediction : within 10 years energy will cost nothing, see ",
+            stepstotake: "Steps to take:"
+
         },
         ua: {
             title:
                 "Superfluid Energy for energy independent houses",
             subtitle:
                 "Quality installations on solar, heatpump and batteries",
-                  course_title:"Cursus zonnepanelen installeren voor Oekraïners",
-            course_text:"Superfuid Energy organiseert samen met Solar Techniek Nederland een cursus Zonnepanelen installeren speciaal voor Oekraïners, met de garantie om binnen 6 weken een goede baan in Nederland te hebben."
-     },
+            goal:
+                "Get rid of russian gas",
+            subgoal:
+                "Prediction : within 10 years energy will cost nothing, see ",
+            stepstotake: "Steps to take:"
+        },
         nl: {
             title:
                 "Superfluid Energy voor energie onafhankelijke woningen",
             subtitle:
                 "Kwaliteits installaties met zonne energie, warmtepompen en batterijen",
-  
-            course_title:"Cursus zonnepanelen installeren voor Oekraïners",
-            course_text:`Superfuid Energy organiseert samen met Solar Techniek Nederland een cursus Zonnepanelen installeren speciaal voor Oekraïners, met de garantie om binnen 6 weken een goede baan in Nederland te hebben.
-            <br><br>De cursus bestaat uit 2 delen:
-            <br>
-1.	 theorie en praktijk van “Monteur op het dak” <br>
-2.	 Theorie en praktijk “vanaf de omvormer tot in de meterkast”,<br><br>
-De cursus wordt gegeven in het Oekraïens.<br>
-Elke cursus wordt afgerond met een examen en een InstallQ certificaat.<br>
-<p>
-De cursus wordt gesubsidieerd door het UWV en de deelnemende bedrijven.
-<p>
-<p>
-Gegarandeerd start salaris  minimaal 2100 euro netto.<p>
-
-<p>De eerste cursus begint op 9 mei 2022</p>
-
-Opgeven via : superfluid.energie@gmail.com
-
-            
-            `
+            goal:
+                "Van het (russisch) gas af.",
+            subgoal:
+                "Voorspelling: Binnen 10 jaar kost energie niets meer, zie  ",
+            stepstotake: "Stappen die genomen moeten worden:"
         },
 
     };
 
     title.textContent = language[lang].title;
     subtitle.textContent = language[lang].subtitle;
- //   goal.textContent = language[lang].goal;
-  //  subgoal.textContent = language[lang].subgoal;
- //   stepstotake.textContent = language[lang].stepstotake;
- course_title.textContent = language[lang].course_title;
- course_text.innerHTML = language[lang].course_text;
+    goal.textContent = language[lang].goal;
+    subgoal.textContent = language[lang].subgoal;
+    stepstotake.textContent = language[lang].stepstotake;
 
 }
 
